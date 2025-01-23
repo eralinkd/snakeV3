f1d9411c0262e06c0ffb1499ed50bc674bece9e0
Богдан <sysoevbogdan82@gmail.com>
2025-01-23 18:09:29 +0200

    images

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,6 +2,9 @@ import SCoin from '@/assets/currency-images/snake-coin.png'
 import Usdt from '@/assets/currency-images/usdt.png'
 import Btc from '@/assets/currency-images/bitcoin.png'
 import Eth from '@/assets/currency-images/eth.png'
+import Bnb from '@/assets/currency-images/bnb.png'
+import Ton from '@/assets/currency-images/ton.jpg'
+import TgStars from '@/assets/currency-images/stars.svg'
 
 export const currencyImages = {
 	SCOIN: SCoin,
@@ -9,5 +12,8 @@ export const currencyImages = {
 	USDT_TRC20: Usdt,
 	BTC: Btc,
 	ETH: Eth,
+	BNB: Bnb,
+	TON: Ton,
+	TG_STARS: TgStars,
 	default: SCoin,
 };
